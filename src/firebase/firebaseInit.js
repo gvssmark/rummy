89664758884146ceff9db29522a02b2a6de11017
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-08-27 06:29:59 +0530

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,4 +1,6 @@
-
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCJvSgo2sC-zyFAFGYZ14SjLKcU2vJ7N_U',
@@ -8,3 +10,7 @@ const firebaseConfig = {
   messagingSenderId: '974097603735',
   appId: '1:974097603735:web:fbda71dae64b4396541dd2',
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
